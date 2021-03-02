45d4a13f40d04e33a1aa036b047fd54be6ceac11
Joao Koritar <jakoritarleite@gmail.com>
2021-03-01 22:27:40 -0300

    Implement the POST request to the Slack Webhook

--- src/lambda/share/main.go
+++ src/lambda/share/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
 
 	"github.com/datareal/repot/src/modules"
 )
+
+type message struct {
+	Message string `json:"message"`
+}
 
 // Handler is used by lambda start which start our code
 func Handler(request modules.Request) (modules.Response, error) {
@@ -22,18 +28,27 @@ func Handler(request modules.Request) (modules.Response, error) {
 		}, 500)
 	}
 
-	_, ok := os.LookupEnv("SLACK_WEEBHOOK_REPORTS")
+	var Message message
+	err := json.Unmarshal([]byte(request.Body), &Message)
+	if err != nil {
+		return modules.CreateResponse(modules.ResponseMessage{
+			Message: "Error when reading request data",
+		}, http.StatusInternalServerError)
+	}
+
+	SlackWebhook, ok := os.LookupEnv("SLACK_WEBHOOK_REPORTS")
 	if !ok {
 		return modules.CreateResponse(modules.ResponseMessage{
 			Message: "Failed loading Slack Webhook URL",
 		}, 500)
 	}
+	fmt.Println("SlackWebhook URL: ", SlackWebhook)
 
-	fmt.Println("Received body: ", request.Body)
+	response, statusCode := modules.PostRequest(Message.Message, SlackWebhook)
 
 	return modules.CreateResponse(modules.ResponseMessage{
-		Message: "Message sent successfuly!",
-	}, 200)
+		Message: response,
+	}, statusCode)
 }
 
 func main() {
