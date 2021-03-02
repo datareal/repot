b896835b54be87e3673be690457c3cbb3937cef4
Joao Koritar <jakoritarleite@gmail.com>
2021-03-01 22:26:40 -0300

    Create Post request function to send the message to the Slack Webhook

--- src/modules/request.go
+++ src/modules/request.go
@@ -1,8 +1,32 @@
 package modules
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
 	"github.com/aws/aws-lambda-go/events"
 )
 
 // Request is used in our lambda functions to define APIGateway Requests
 type Request events.APIGatewayProxyRequest
+
+// PostRequest is used to create the Request to the Slack Webhook url
+func PostRequest(message string, SlackWebhook string) (string, int) {
+	bytesRepresentation, err := json.Marshal(map[string]interface{}{
+		"text": message,
+	})
+	if err != nil {
+		fmt.Println("json:Marshal::Error ", err)
+		return "Error when creating the Slack form", 500
+	}
+
+	resp, err := http.Post(SlackWebhook, "application/json", bytes.NewBuffer(bytesRepresentation))
+	if err != nil {
+		fmt.Println("http:Post::Error: ", err)
+		return "Error when making the POST request to the Slack Webhook URL", 500
+	}
+
+	return "Message created successfuly!", 200
+}
