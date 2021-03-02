bfb711d22b56fbcfdf88e1fd3d2fb8e70bad3f31
Joao Koritar <jakoritarleite@gmail.com>
2021-03-01 21:11:49 -0300

    Create response.ResponseMessage struct and now return the json<ResponseMessage>

--- src/modules/response.go
+++ src/modules/response.go
@@ -10,13 +10,16 @@ import (
 // Response is used in our lambda functions to define APIGateway Response
 type Response events.APIGatewayProxyResponse
 
+// ResponseMessage is used in our lambda function to return when there's a message to be returned
+type ResponseMessage struct {
+	Message string
+}
+
 // CreateResponse is used to create a response to the received request
-func CreateResponse(message string, responseStatusCode int) (Response, error) {
+func CreateResponse(message ResponseMessage, responseStatusCode int) (Response, error) {
 	var buf bytes.Buffer
 
-	body, err := json.Marshal(map[string]interface{}{
-		"Message": message,
-	})
+	body, err := json.Marshal(message)
 	if err != nil {
 		return Response{StatusCode: 500, Body: "Failed to create the Request body"}, err
 	}
