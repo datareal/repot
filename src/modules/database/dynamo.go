13b1cd8af0b78f7d4d27a36943c6d1074a023036
Joao Koritar <jakoritarleite@gmail.com>
2021-03-22 15:49:49 -0300

    Implement ExclusiveStartKey usage on Scan function

--- src/modules/database/dynamo.go
+++ src/modules/database/dynamo.go
@@ -10,13 +10,13 @@ import (
 )
 
 const awsRegion = "us-east-1"
-const tableName = "datareal-crawler-reports"
+const tableName = "datareal-crawler-unique-rawdata"
 
 var database = dynamodb.New(session.New(), aws.NewConfig().WithRegion(awsRegion))
 
 // Item is the item structure from DynamoDB `datareal-crawler-reports` table
 type Item struct {
-	State string `json:"state"`
+	Status string `json:"status"`
 }
 
 func queryItems(Date string, LastEvaluatedKey map[string]*dynamodb.AttributeValue) ([]Item, map[string]*dynamodb.AttributeValue, error) {
@@ -78,24 +78,49 @@ func QueryAll(Date string) ([]Item, error) {
 	return result, nil
 }
 
-// ScanItems retrieves all the items from the desired table
-func ScanItems() ([]Item, error) {
+func scanItems(LastEvaluatedKey map[string]*dynamodb.AttributeValue) ([]Item, map[string]*dynamodb.AttributeValue, error) {
 	input := &dynamodb.ScanInput{
-		TableName: aws.String(tableName),
+		TableName:         aws.String(tableName),
+		ExclusiveStartKey: LastEvaluatedKey,
 	}
 	result, err := database.Scan(input)
 	if err != nil {
-		return []Item{}, err
+		return []Item{}, nil, err
 	}
 	if len(result.Items) == 0 {
-		return []Item{}, nil
+		return []Item{}, nil, nil
 	}
+	fmt.Println("Last EvaluatedKey from scan is", result.LastEvaluatedKey)
 
 	var items []Item
 	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &items)
 	if err != nil {
-		return []Item{}, err
+		return []Item{}, nil, err
 	}
 
-	return items, nil
+	return items, result.LastEvaluatedKey, nil
+}
+
+// ScanAll retrieves all the items from the desired table
+func ScanAll() ([]Item, error) {
+	var result []Item
+	var PaginationDone bool = false
+	var ExclusiveStartKey map[string]*dynamodb.AttributeValue = nil
+
+	for !PaginationDone {
+		items, LastEvaluatedKey, err := scanItems(ExclusiveStartKey)
+		if err != nil {
+			return []Item{}, err
+		}
+		result = append(result, items...)
+
+		if LastEvaluatedKey == nil {
+			fmt.Println("The final result lenght is", len(result))
+			PaginationDone = true
+		} else {
+			ExclusiveStartKey = LastEvaluatedKey
+		}
+	}
+
+	return result, nil
 }
