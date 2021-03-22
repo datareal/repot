7416addf5b9588c8445fa34f43a27333b8a383fc
Joao Koritar <jakoritarleite@gmail.com>
2021-03-22 15:53:30 -0300

    Change some types from APIGateway to ours

--- src/modules/response.go
+++ src/modules/response.go
@@ -3,12 +3,14 @@ package modules
 import (
 	"bytes"
 	"encoding/json"
-
-	"github.com/aws/aws-lambda-go/events"
 )
 
 // Response is used in our lambda functions to define APIGateway Response
-type Response events.APIGatewayProxyResponse
+type Response struct {
+	StatusCode int               `json:"StatusCode"`
+	Body       string            `json:"Body"`
+	Headers    map[string]string `json:"Headers"`
+}
 
 // ResponseMessage is used in our lambda function to return when there's a message to be returned
 type ResponseMessage struct {
@@ -26,9 +28,8 @@ func CreateResponse(message ResponseMessage, responseStatusCode int) (Response, 
 	json.HTMLEscape(&buf, body)
 
 	resp := Response{
-		StatusCode:      responseStatusCode,
-		IsBase64Encoded: false,
-		Body:            buf.String(),
+		StatusCode: responseStatusCode,
+		Body:       buf.String(),
 		Headers: map[string]string{
 			"Content-Type":           "application/json",
 			"X-MyCompany-Func-Reply": "world-handler",
